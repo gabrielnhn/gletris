2da827dce3c5b46275321aca5e14a5428b7156e8
Gabriel Hishida <gabrielnhn@duck.com>
2025-03-02 21:20:36 +0100

    implement joystick movement

--- main.cpp
+++ main.cpp
@@ -2,25 +2,19 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
-#include <string>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp> 
 
-#include "npy.hpp"
-
-
 # define M_PI 3.14159265358979323846
 
-float fov = glm::radians(45.0f);
-// float farDistance=50.0f;
-float farDistance=1.2f;
-// float farDistance=0.5f;
-float nearDistance=0.1f;
 
-auto camera = glm::vec3(-0.5, -0.5, 1);
-auto aim = glm::vec3(-0.5, -0.5, 0);
+int axes_count;
+const float* joystick_axes;
+
+float farDistance=50.0f;
+auto camera = glm::vec3(0, 0, 3);
+auto aim = glm::vec3(0, 0, 0);
 
 double mousex, mousey;
 double mousex_last, mousey_last;
@@ -29,64 +23,64 @@ int last_mouse_event = GLFW_RELEASE;
 double height = 800;
 double width = 800;
 
-// 346x260
-float ds_width = 346;
-float ds_height = 260;
-
 float speed = 0.02f;
 
-// static float yaw = 90.0f; // Start facing backward?
-static float yaw = -90.0f; 
+static float yaw = -90.0f; // Start facing forward
 static float pitch = 0.0f;
 glm::mat4 mvp;
 
-float aspect_ratio = width/height;
+glm::mat3x3 right_transform = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(0, 1, 0));
 
 
-void framebuffer_size_callback(GLFWwindow* window, int w, int h)
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    width = w;
-    height = h;
     glViewport(0, 0, width, height);
-    std::cout << "CHANGED SIZE";
-    std::cout << aspect_ratio << std::endl;
-    aspect_ratio = width/height;
-    std::cout << aspect_ratio << std::endl;
-
 }  
 
 void processInput(GLFWwindow *window)
 {
+
+    float joystick_threshold = 0.3f;
+
+    joystick_axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axes_count); 
+
+    std::cout << "joystick_axes " << axes_count << std::endl;
+    for(int k = 0; k < axes_count; k++)
+    {
+        std::cout << "\t axis " << k << ": " << joystick_axes[k] << std::endl;
+    }
+
+
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
     
-    glm::vec3 forward = glm::normalize(aim - camera);
-    // glm::vec3 right = glm::normalize(glm::cross(glm::vec3(0, 1, 0),forward)); // Right vector
-    glm::vec3 right = glm::normalize(glm::cross(forward, glm::vec3(0, 1, 0))); // Right vector
+    glm::vec3 forward = glm::normalize(aim - camera); // Forward direction
+    glm::vec3 right = right_transform * forward; // Forward direction
 
 
-    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    if((glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) or (joystick_axes[1] > joystick_threshold))
     {
         camera -= speed * forward;
-        // aim -= speed * forward;
+        aim -= speed * forward;
     }
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS or (joystick_axes[1] < -joystick_threshold))
     {
         camera += speed * forward;
-        // aim += speed * forward;
+        aim += speed * forward;
     }
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS or (joystick_axes[0] > joystick_threshold))
     {
         camera += speed * right;
-        // aim += speed * right;
+        aim += speed * right;
     }
-    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS or (joystick_axes[0] < -joystick_threshold))
     {
         camera -= speed * right;
-        // aim -= speed * right;
+        aim -= speed * right;
     }
-    aim = camera + forward;
-
 
     glfwGetCursorPos(window, &mousex, &mousey);
    
@@ -119,84 +113,21 @@ void processInput(GLFWwindow *window)
 
             aim = camera + direction;
             
+            
+            // std::cout << "xdiff" << xdiff << std::endl;
+
             mousex_last = mousex;
             mousey_last = mousey;
         }
         
     }
-    // std::cout << "AIM: " << aim.x << ", " << aim.y << ", " << aim.z << ", (mousex =" << mousex << std::endl;
-    // std::cout << "CAM: " << camera.x << ", " << camera.y << ", " << camera.z << ", (mousex =" << mousex << std::endl;
-
-    //clamp
-    camera.x = std::clamp(camera.x, -1.0f, 2.0f);
-    aim.x = std::clamp(aim.x, -1.0f, 2.0f);
-
-    camera.y = std::clamp(camera.y, -1.0f, 2.0f);
-    aim.y = std::clamp(aim.y, -1.0f, 2.0f);
+    std::cout << "AIM: " << aim.x << ", " << aim.y << ", " << aim.z << ", (mousex =" << mousex << std::endl;
 }
+
+
 
 int main()
 {
-
-    // const std::string path = "data/A0P10C0-2021_11_04_12_30_08.npy";
-    // const std::string path = "data/A0P10C1-2021_11_04_12_30_17.npy";
-    // const std::string path = "data/A0P11C0-2021_11_04_13_09_56.npy";
-    // const std::string path = "data/A1P10C0-2021_11_04_12_30_45.npy";
-    const std::string path = "data/A2P10C0-2021_11_04_12_31_28.npy";
-    // const std::string path = "data/A10P10C0-2021_11_04_12_34_46.npy";
-    // const std::string path = "data/A54P25C1-2021_11_08_11_34_37.npy";
-    
-    std::cout << "READING: " << path << std::endl;
-    npy::npy_data d = npy::read_npy<float>(path);
-    d.fortran_order = true;
-
-    std::vector<float> data = d.data;
-    std::vector<unsigned long> shape = d.shape;
-
-    // std::cout << "shape: ";
-    // for(auto val: shape)
-    //     std::cout << val << ", ";
-
-    // std::cout << std::endl;
-    // std::cout << "BUT FLOAT VECTOR IS SIZE " << data.size() << std::endl;
-    
-    // for(int j = 0; j < 4; j++)
-    // {
-    //     int i = j * shape[0];
-    //     std::cout << data[i] << ", " ;
-    // }
-    
-    std::cout << std::endl;
-    // return 0;
-
-    std::vector<float> vertices; // Create a vector of glm::vec4
-
-    // declare start
-    // for(int x = 0; x < ds_width; x++)
-    // {
-    //     for(int y = 0; y < ds_height; y++)
-    //     {
-    //         vertices.push_back(x);
-    //         vertices.push_back(y);
-    //         vertices.push_back(0);
-    //         if (x == 0)
-    //         vertices.push_back(0);
-    //         else
-    //         vertices.push_back(1);
-    //     }
-    // }
-
-
-
-    // Convert the column-major data into row-major
-    for (size_t i = 0; i < shape[0]; ++i) {
-        for (size_t j = 0; j < shape[1]; ++j) {
-            size_t index = j * shape[0] + i;  // Convert column-major index
-            vertices.push_back(data[index]); // Convert to vec4
-        }
-    }
-
-
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -220,14 +151,32 @@ int main()
     glViewport(0, 0, width, height);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
-    aspect_ratio = width/height;
-    glm::mat4 projection = glm::perspective(fov, aspect_ratio, nearDistance, farDistance);
+
+
+    std::vector<glm::vec4> vertices = {
+        {0.1+ -0.3f, 0.3f, -1.0f, 1.0},
+        {0.1+ 0.3f,  0.3f, -1.0f, 1.0},
+        {0.1+ -0.3f, -0.3f, -1.0f, 1.0},
+        {0.1+ 0.3f, -0.3f, -1.0f, 1.0},
+        {0.1+ -0.3f, 0.3f, -0.7f, 1.0},
+        {0.1+ 0.3f,  0.3f, -0.7f, 1.0},
+        {0.1+ -0.3f, -0.3f, -0.7f, 1.0},
+        {0.1+ 0.3f, -0.3f, -0.7f, 1.0},
+    };  
+
+    //fuck it perspective
+
+    glm::mat4 projection = glm::perspective(glm::radians(30.0f), 1.0f, 0.1f, farDistance);
 
     glm::mat4 view = glm::lookAt(camera, aim, glm::vec3(0, 1, 0));
-    // view = flipZ* view;
+
 
     glm::mat4 model = glm::mat4(1.0f);
+
     mvp = projection * view * model;
+    
+
+    std::vector<glm::vec3> gl_vertices;
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
@@ -238,53 +187,48 @@ int main()
 
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);  
-
-     // Normalize vertex data to fit within [-1, 1] range
-     for (size_t i = 0; i < vertices.size(); i += 4) {
-        vertices[i] /= -ds_width;     // Scale x
-        vertices[i + 1] /= -ds_height; // Scale y
-        vertices[i + 2] /= -500000.0f; // Scale z
-    }
+    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(glm::vec4), &vertices.front(), GL_DYNAMIC_DRAW);
 
 
-    for (size_t i = 0; i < 10; ++i) {
-        std::cout << "Vertex " << i << ": "
-                  << vertices[i * 4] << ", "
-                  << vertices[i * 4 + 1] << ", "
-                  << vertices[i * 4 + 2] << ", "
-                  << vertices[i * 4 + 3] << std::endl;
-    }
-
-    // glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(glm::vec4), &vertices.front(), GL_DYNAMIC_DRAW);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(float), &vertices.front(), GL_DYNAMIC_DRAW);
-
-    // glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4), (void*)0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(float)*4, (void*)0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4), (void*)0);
     glEnableVertexAttribArray(0);  
 
-    
-    const char *vertexShaderSourceGLSLCode =
-        "#version 330 core\n"
-        "layout (location = 0) in vec4 vertexPosition; // Expecting vec4 for each vertex\n"
-        "uniform mat4 mvp;  // Model-View-Projection matrix\n"
-        "out float vertexColor;  // Output color to fragment shader\n"
+    uint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    std::vector<int> indices = {
+        0,1,2,
+        1,2,3,
+        0,4,6,
+        0,2,6,
+        2,6,7,
+        2,3,7,
+        1,3,7,
+        1,5,7,
+        0,1,4,
+        0,1,5,
+        4,5,6,
+        5,6,7,
+    };
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()*sizeof(int), &indices.front(), GL_DYNAMIC_DRAW); 
+
+    const char *vertexShaderSourceGLSLCode = "#version 330 core\n"
+        // "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 0) in vec4 aPos;\n"
+        "uniform mat4 mvp;\n"
         "void main()\n"
         "{\n"
-        "    vertexColor = vertexPosition.w;\n"  // Pass the position directly to the fragment shader for color"
-        "    gl_Position = mvp * vec4(vertexPosition.xyz, 1.0);\n"  // Apply MVP transformation"
-        // "    gl_Position = mvp * vec4(vertexPosition.y, vertexPosition.x,z, 1.0);\n"  // Apply MVP transformation"
+        // "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        // "   gl_Position = mvp * vec4    (aPos, 1.0);\n"
+        "   gl_Position = mvp * aPos;\n"
         "}\0";
-
     
     const char *fragShaderSourceGLSLCode = "#version 330 core\n"
         "out vec4 FragColor;\n"
-        "in float vertexColor;\n"
         "void main()\n"
         "{\n"
-            "if(vertexColor > 0.5)\n"
-            "FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
-            "else\n"
-            "FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+            "FragColor = vec4(0.2f, 0.9f, 0.2f, 1.0f);\n"
         "}\0";
     
 
@@ -310,25 +254,68 @@ int main()
     int mvpLocation = glGetUniformLocation(shaderProgram, "mvp");
     glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, glm::value_ptr(mvp));
 
-    glUniform1i(glGetUniformLocation(shaderProgram, "total_vertices"), vertices.size());
-    glUniform1i(glGetUniformLocation(shaderProgram, "column_size"), vertices.size()/4);
-
 
     glEnable(GL_DEPTH_TEST);
 
+    // int i = 0;
+    // float vx = 0.012;
+    float vx = 0;
+    // float vy = 0.013;
+    float vy = 0;
+    // bool goingright = true;
+    int j = 0;
     glClearColor(0.2f, 0.2f, 0.2f, 0.5f);
     while(!glfwWindowShouldClose(window))
     {
 
-        GLenum err;
-        while ((err = glGetError()) != GL_NO_ERROR) {
-            std::cerr << "OpenGL error: " << err << std::endl;
+        j++;
+        // if (j % 5 == 0)
+        if (true)
+        {
+            const float BOUND = 5.0f;
+
+            if (vertices[2].y < -BOUND)
+            {
+                vy = -vy;
+                glClearColor(0.8f, 0.2f, 0.2f, 1.0f);
+            }
+                
+            if (vertices[0].y >= BOUND)
+            {
+                vy = - vy;
+                glClearColor(0.2f, 0.5f, 0.2f, 1.0f);
+            }
+
+            if (vertices[1].x > BOUND)
+            {
+                vx = -vx;
+                glClearColor(0.3f, 0.5f, 0.8f, 1.0f);
+
+            }
+
+            if (vertices[0].x < -BOUND)
+            {
+                vx = -vx;
+                glClearColor(0.9f, 0.3f, 0.0f, 1.0f);
+            }
+
+            for(long unsigned int k=0; k < vertices.size(); k++)
+            {
+                vertices[k].x += vx;
+                vertices[k].y += vy;
+             
+                // std::cout << k << ": " << vertices[k].y << std::endl;
+                
+            }
+
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(glm::vec4), &vertices.front(), GL_DYNAMIC_DRAW);
+
         }
 
         // remake projection
-        glm::mat4 projection = glm::perspective(fov, aspect_ratio, nearDistance, farDistance);
         view = glm::lookAt(camera, aim, glm::vec3(0, 1, 0));
-        mvp = projection * view * model;
+        glm::mat4 mvp = projection * view * model;
         int mvpLocation = glGetUniformLocation(shaderProgram, "mvp");
         glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, glm::value_ptr(mvp));
 
@@ -338,12 +325,8 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        // glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-        // glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-        // glDrawArrays(GL_POINTS, 0, vertices.size());  // Each vertex is 1 float
-        glPointSize(1.0f); // Set point size to 10 pixels
-        // glPointSize(3.0f); // Set point size to 10 pixels
-        glDrawArrays(GL_POINTS, 0, vertices.size()/4);  // Each vertex is 1 float
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();    
